3df3d85420da10effa92cd816ee65bcc831d3289
gpicha3 <gamepichaya2@gmail.com>
2023-12-19 09:00:42 +0700

    Question 1/4: clock angle refactor

--- clock-angle.ts
+++ clock-angle.ts
@@ -1,26 +1,11 @@
-const getClockAngle = (time: string) => {
-    let Hour = Number(time.split(":")[0]);
-    let Minute = Number(time.split(":")[1]);
+const getClockAngle = (time) => {
+    const [hour, minute] = time.split(":").map(Number);
+    let hourAngle = (hour % 12 + minute / 60) * 30;
+    let minuteAngle = minute * 6;
 
-    let CalHour = Hour > 12? Hour - 12 : Hour;
-    let AngleMinute = Minute*6;
-    let tolerance = (Minute*100)/60;
-    let AngleHour = (CalHour+(tolerance/100))*30;
-
-    if(AngleHour > 180){
-        AngleHour = 360-AngleHour;
-        AngleMinute = AngleMinute > 180? 360-AngleMinute : AngleMinute;
-        return Math.abs(AngleHour-AngleMinute);
-    }
-
-    if(AngleMinute > 180 ){
-        AngleMinute = 360-AngleMinute
-        AngleHour = AngleHour < 180? 180-AngleHour: AngleHour;
-        return Math.abs(AngleHour+AngleMinute);
-    }
-
-    return Math.abs(AngleHour-AngleMinute);
-}
+    const angleDiff = Math.abs(hourAngle - minuteAngle);
+    return angleDiff > 180 ? 360 - angleDiff : angleDiff;
+};
 
 getClockAngle("09:00")
 getClockAngle("17:30")
